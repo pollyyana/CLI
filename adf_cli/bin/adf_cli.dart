52bd93044406cb0c0658a3056e873f70d61b0457
pollyyana <pollyana.medeirosk@gmail.com>
2023-07-13 03:12:24 -0300

    tratando parametros de entrada (args)

--- adf_cli/bin/adf_cli.dart
+++ adf_cli/bin/adf_cli.dart
@@ -1,4 +1,39 @@
-import 'package:adf_cli/adf_cli.dart' as adf_cli;
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
 
+//terminal
+//  dart ./bin/adf_cli.dart exemplo
+// dart ./bin/adf_cli.dart exemplo -t console_full
 void main(List<String> arguments) {
+  // final argparse = ArgParser();
+  // argparse.addFlag('data', abbr: 'd');
+  // argparse.addOption('name', abbr: 'n');
+  // argparse.addOption('template', abbr: 't');
+  // final argResult = argparse.parse(arguments);
+
+  // print('${argResult['data']}');
+  // print('${argResult['name']}');
+  // print('${argResult['template']}');
+
+  CommandRunner('ADF CLI', 'ADF CLI')
+    ..addCommand(ExemploCommand())
+    ..run(arguments);
+}
+
+class ExemploCommand extends Command {
+  @override
+  String get description => 'Exemplo de comando';
+
+  @override
+  String get name => 'exemplo';
+
+  ExemploCommand() {
+    argParser.addOption('template', abbr: 't');
+  }
+
+  @override
+  run() {
+    print(argResults?['template']);
+    print('executar qualquer coisa');
+  }
 }
---
+++ adf_cli/lib/src/commands/students/students_command.dart
@@ -0,0 +1,15 @@
+
+import 'package:args/command_runner.dart';
+
+class StudentsCommand extends Command {
+  @override
+  // TODO: implement description
+  String get description => throw UnimplementedError();
+
+  @override
+  // TODO: implement name
+  String get name => throw UnimplementedError();
+  
+}
+
+
